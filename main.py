90f66f247a14f982160de207faa28b73fbb24f7c
Aditya Bansal <adityabansal10@gmail.com>
2023-09-17 15:09:12 -0400

    adds code

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyTorch_CIFAR10.cifar10_models.resnet import resnet18
+from torchsummary import summary
+
+# Untrained model
+# my_model = vgg11_bn()
+# my_model = restnet18()
+
+# Pretrained model
+my_model = resnet18(pretrained=True)
+my_model.eval() # for evaluation
+# print(my_model)
+summary(model=my_model, input_size=(3, 224, 224), batch_size=1, device='cpu')
